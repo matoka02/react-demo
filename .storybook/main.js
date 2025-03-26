e0d0a3333a539c7af05c92e226825a94981d5333
Anastasiia <120492521+matoka02@users.noreply.github.com>
2025-03-26 11:37:05 +0200

    update storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,14 @@
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  framework: '@storybook/react',
-  core: {
-    builder: '@storybook/builder-webpack5',
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+
+  framework: {
+    name: "@storybook/nextjs",
+    options: {}
   },
+
+  docs: {
+    autodocs: true
+  },
+
+  addons: ['@storybook/addon-webpack5-compiler-babel', '@chromatic-com/storybook']
 };
