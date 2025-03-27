771f9790c800ab74e7ad85f17ae03e14712a3f3c
Anastasiia <120492521+matoka02@users.noreply.github.com>
2025-03-27 21:05:46 +0200

    create components
    - Logo;

---
+++ src/components/logo/classes.ts
@@ -0,0 +1,7 @@
+// ----------------------------------------------------------------------
+
+const logoClasses = {
+  root: 'mnl__logo__root',
+};
+
+export default logoClasses;
---
+++ src/components/logo/index.ts
@@ -0,0 +1,3 @@
+export { default as Logo } from './Logo';
+
+export { default as logoClasses } from './classes';
