cfad4181c964217bda4fb3ef9e293bd0bf60d37c
Anastasiia <120492521+matoka02@users.noreply.github.com>
2025-03-29 14:36:33 +0200

    create components
    - CustomerTableHead;
    - CustomerTableRows;
    - CustomerTableToolbar;

---
+++ src/components/customer/CustomerTableRows.tsx
@@ -0,0 +1,148 @@
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import MenuItem, { menuItemClasses } from '@mui/material/MenuItem';
+import MenuList from '@mui/material/MenuList';
+import Popover from '@mui/material/Popover';
+import TableCell from '@mui/material/TableCell';
+import TableRow from '@mui/material/TableRow';
+import { useState, useCallback } from 'react';
+
+import { useRouter } from '@/routes/hooks';
+import { CUSTOMER_DURATION } from '@/stores/customers/customerSlice';
+import { deleteCustomer } from '@/stores/customers/customerThunk';
+
+import { Iconify } from '../iconify';
+import { Label } from '../label';
+
+// ----------------------------------------------------------------------
+
+type CustomerTableRowProps = {
+  row: Customer;
+  selected: boolean;
+  onSelectRow: () => void;
+  toggleNotice: (open: boolean) => void;
+  onDialogConfirm: (message?: string) => Promise<boolean>;
+};
+
+function CustomerTableRow({
+  row,
+  selected,
+  onSelectRow,
+  toggleNotice,
+  onDialogConfirm,
+}: CustomerTableRowProps): React.ReactElement {
+  const [openPopover, setOpenPopover] = useState<HTMLButtonElement | null>;
+  const [customerId, setCustomerId] = useState('');
+  const router = useRouter();
+
+  const handleOpenPopover = useCallback(
+    (evt: React.MouseEvent<HTMLButtonElement> & TODO) => {
+      setOpenPopover(evt.currentTarget);
+      setCustomerId(evt.currentTarget.value);
+    },
+    [setOpenPopover, setCustomerId]
+  );
+
+  const handleEditing = useCallback(() => {
+    // console.log(` openPopover id: ${customerId}`);
+    setOpenPopover(null);
+    router.push(`/edit-customer/${customerId}`);
+  }, [customerId,setOpenPopover, router]);
+
+  const handleDelete = useCallback(async () => {
+    // console.log(` openPopover id: ${customerId}`);
+    const deleteConfirmed = await onDialogConfirm();
+    if (deleteConfirmed) {
+      deleteCustomer(customerId as TODO);
+
+      toggleNotice(true);
+      setTimeout(() => {
+        router.push('/customers');
+        toggleNotice(false);
+      }, CUSTOMER_DURATION);
+    }
+    setOpenPopover(null);
+  }, [customerId, router]);
+
+  return (
+    <>
+      {/** Table */}
+      <TableRow hover tabIndex={-1} role="checkbox" selected={selected}>
+        <TableCell padding="checkbox">
+          <Checkbox disableRipple checked={selected} onChange={onSelectRow} />
+        </TableCell>
+
+        <TableCell component="th" scope="row">
+          <Box gap={2} display="flex" alignItems="center">
+            <Avatar alt={row.name} src={row.avatarUrl} />
+            {row.name}
+          </Box>
+        </TableCell>
+
+        {/* <TableCell>{row.company}</TableCell> */}
+        <TableCell>{row.email}</TableCell>
+        <TableCell>{row.mobile}</TableCell>
+        <TableCell>{row.phone}</TableCell>
+        {/* <TableCell>{row.billingAddress}</TableCell> */}
+
+        <TableCell align="center">
+          {row.hasItemInShoppingCart ? (
+            <Iconify width={22} icon="solar:check-circle-bold" sx={{ color: 'success.main' }} />
+          ) : (
+            '-'
+          )}
+        </TableCell>
+
+        <TableCell>
+          <Label color={(row.membership === 'vip' && 'warning') || 'info'}>
+            {row.membership.toUpperCase()}
+          </Label>
+        </TableCell>
+
+        <TableCell align="right">
+          <IconButton onClick={handleOpenPopover} value={row.id}>
+            <Iconify icon="eva:more-vertical-fill" />
+          </IconButton>
+        </TableCell>
+      </TableRow>
+
+{/** Delete dialog */}
+      <Popover
+        open={!!openPopover}
+        anchorEl={openPopover}
+        onClose={handleEditing}
+        anchorOrigin={{ vertical: 'top', horizontal: 'left' }}
+        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      >
+        <MenuList
+          disablePadding
+          sx={{
+            p: 0.5,
+            gap: 0.5,
+            width: 140,
+            display: 'flex',
+            flexDirection: 'column',
+            [`& .${menuItemClasses.root}`]: {
+              px: 1,
+              gap: 2,
+              borderRadius: 0.75,
+              [`&.${menuItemClasses.selected}`]: { bgcolor: 'action.selected' },
+            },
+          }}
+        >
+          <MenuItem onClick={handleEditing}>
+            <Iconify icon="solar:pen-bold" />
+          </MenuItem>
+
+          <MenuItem onClick={handleDelete} sx={{ color: 'error.main' }}>
+            <Iconify icon="solar:trash-bin-trash-bold" />
+          </MenuItem>
+        </MenuList>
+      </Popover>
+    </>
+  );
+}
+
+export default CustomerTableRow;
