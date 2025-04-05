f1e799927bb9f0ebc4da8a4ebb697966dc54ab76
Anastasiia <120492521+matoka02@users.noreply.github.com>
2025-04-05 19:57:39 +0300

    fix RouterLink

--- src/routes/components/RouterLink.tsx
+++ src/routes/components/RouterLink.tsx
@@ -1,31 +1,39 @@
-import { Link as MuiLink, LinkProps as MuiLinkProps } from '@mui/material';
+import {
+  // Link as MuiLink,
+  LinkProps as MuiLinkProps,
+} from '@mui/material';
 import NextLink, { LinkProps as NextLinkProps } from 'next/link';
 import React from 'react';
 
 // ----------------------------------------------------------------------
 
-interface RouterLinkProps extends Omit<MuiLinkProps, 'href'>, Omit<NextLinkProps, 'href' | 'as'> {
+// interface RouterLinkProps extends Omit<MuiLinkProps, 'href'>,
+// Omit<NextLinkProps, 'href' | 'as'> {
+//   href: string;
+//   as?: string;
+// }
+interface RouterLinkProps extends Omit<MuiLinkProps, 'href'>, Omit<NextLinkProps, 'href'> {
   href: string;
-  as?: string;
 }
 
 function RouterLinkComponent(
-  { href, as, children, ...other }: RouterLinkProps,
+  { href, children, ...other }: RouterLinkProps,
   ref: React.Ref<HTMLAnchorElement>
 ): React.ReactElement {
   return (
-    <NextLink href={href} as={as} passHref legacyBehavior>
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <NextLink href={href} ref={ref} {...other}>
       {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-      <MuiLink ref={ref} {...other}>
-        {children}
-      </MuiLink>
+      {/* <MuiLink ref={ref} {...other}> */}
+      {children}
+      {/* </MuiLink> */}
     </NextLink>
   );
 }
 
-RouterLinkComponent.defaultProps = {
-  as: undefined,
-};
+// RouterLinkComponent.defaultProps = {
+//   as: undefined,
+// };
 
 RouterLinkComponent.displayName = 'RouterLink';
 
