6e7107b6b6098f9061519dc16ea7b8bb128a43e4
Anastasiia <120492521+matoka02@users.noreply.github.com>
2025-04-06 14:09:37 +0300

    update useForm

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,22 +1,9 @@
-// import { useTheme } from '@mui/material';
 import { useEffect, useState } from 'react';
-
-// const getStyles = (theme: TODO) => ({
-//   root: {
-//     '& .MuiFormControl-root': {
-//       width: '80%',
-//       margin: (theme as TODO).spacing(1),
-//     },
-//   },
-// });
 
 function useForm<T extends object>(initialFieldValues: T, selectedData: T) {
   const [values, setValues] = useState<T>(selectedData || initialFieldValues);
   const [errors, setErrors] = useState<Partial<Record<keyof T, string>>>({});
   const [currentField, setCurrentField] = useState<string>('');
-
-  // const theme = useTheme();
-  // const classes = getStyles(theme);
 
   const handleInputChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
     // const { name, value } = evt.target;
@@ -26,7 +13,7 @@ function useForm<T extends object>(initialFieldValues: T, selectedData: T) {
       // [name]: value,
       [name]: type === 'checkbox' ? checked : value,
     });
-    // console.log("inputChange");
+    console.log("inputChange: ", value);
     setCurrentField(name);
   };
 
