d011a755dfa1e17dd2613bbc2f58ca95bc23e2ef
Anastasiia <120492521+matoka02@users.noreply.github.com>
2025-04-07 22:50:06 +0300

    fix store

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import agentReducer from './agents/agentSlice';
 import customerReducer from './customers/customerSlice';
+import orderReducer from './orders/orderSlice';
 
 export const store = configureStore({
   reducer: {
     agents: agentReducer,
     customers: customerReducer,
+    orders: orderReducer,
   },
   devTools: process.env.NODE_ENV !== 'production',
 });
