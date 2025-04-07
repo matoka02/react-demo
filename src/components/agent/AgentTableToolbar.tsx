e75abde0a48b03141e9eb89469011bbdd4c66b1b
Anastasiia <120492521+matoka02@users.noreply.github.com>
2025-04-07 23:05:57 +0300

    fix search in useTable

--- src/components/agent/AgentTableToolbar.tsx
+++ src/components/agent/AgentTableToolbar.tsx
@@ -45,7 +45,7 @@ function AgentTableToolbar({
           fullWidth
           value={filterName}
           onChange={onFilterName}
-          placeholder="Search user..."
+          placeholder="Search name..."
           startAdornment={
             <InputAdornment position="start">
               <Iconify width={20} icon="eva:search-fill" sx={{ color: 'text.disabled' }} />
--- src/components/table/utils.ts
+++ src/components/table/utils.ts
@@ -76,9 +76,11 @@ export function applyFilter({ inputData, comparator, filterName }: ApplyFilterPr
   let filteredData = stabilizedThis.map((el) => el[0]);
 
   if (filterName) {
-    filteredData = filteredData.filter((user: TODO) =>
-      user.name.toLowerCase().includes(filterName.toLowerCase())
-    ) as TODO;
+    filteredData = filteredData.filter((item: any) => {
+      const valueToSearch = 'name' in item ? item.name : 'customer' in item ? item.customer : '';
+
+      return valueToSearch?.toLowerCase().includes(filterName.toLowerCase());
+    });
   }
 
   return filteredData;
