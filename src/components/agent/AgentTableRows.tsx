13f10ee7672c71cda1bb45e560ebebc85115e4dd
Anastasiia <120492521+matoka02@users.noreply.github.com>
2025-04-07 16:37:54 +0300

    add components

---
+++ src/components/agent/AgentTableRows.tsx
@@ -0,0 +1,146 @@
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import MenuItem, { menuItemClasses } from '@mui/material/MenuItem';
+import MenuList from '@mui/material/MenuList';
+import Popover from '@mui/material/Popover';
+import TableCell from '@mui/material/TableCell';
+import TableRow from '@mui/material/TableRow';
+import { useState, useCallback } from 'react';
+
+import { useAppRouter } from '@/routes/hooks';
+import { AGENT_DURATION } from '@/stores/agents/agentSlice';
+import { deleteAgent } from '@/stores/agents/agentThunk';
+import { useAppDispatch } from '@/stores/hooks';
+
+import { Iconify } from '../iconify';
+import { Label } from '../label';
+
+// ----------------------------------------------------------------------
+
+type AgentTableRowProps = {
+  row: Agent;
+  selected: boolean;
+  onSelectRow: () => void;
+  // toggleNotice: (open: boolean) => void;
+  onDialogConfirm: (message?: string) => Promise<boolean>;
+};
+
+function AgentTableRow({
+  row,
+  selected,
+  onSelectRow,
+  // toggleNotice,
+  onDialogConfirm,
+}: AgentTableRowProps): React.ReactElement {
+  const dispatch = useAppDispatch();
+  const [openPopover, setOpenPopover] = useState<HTMLButtonElement | null>(null);
+  const [agentId, setAgentId] = useState('');
+  const appRouter = useAppRouter();
+
+  const handleOpenPopover = useCallback(
+    (evt: React.MouseEvent<HTMLButtonElement>) => {
+      setOpenPopover(evt.currentTarget);
+      setAgentId(evt.currentTarget.value);
+    },
+    [setOpenPopover, setAgentId]
+  );
+
+  const handleEditing = useCallback(() => {
+    setOpenPopover(null);
+    appRouter.push(`/agents/form/${agentId}`);
+  }, [agentId, setOpenPopover, appRouter]);
+
+  const handleDelete = useCallback(async () => {
+    const deleteConfirmed = await onDialogConfirm();
+    if (deleteConfirmed) {
+      dispatch(deleteAgent(agentId));
+
+      setTimeout(() => {
+        appRouter.push('/agents');
+      }, AGENT_DURATION);
+    }
+    setOpenPopover(null);
+  }, [agentId, dispatch, appRouter, onDialogConfirm]);
+
+  return (
+    <>
+      {/** Table */}
+      <TableRow hover tabIndex={-1} role="checkbox" selected={selected}>
+        <TableCell padding="checkbox">
+          <Checkbox disableRipple checked={selected} onChange={onSelectRow} />
+        </TableCell>
+
+        <TableCell component="th" scope="row">
+          <Box gap={2} display="flex" alignItems="center">
+            <Avatar alt={row.name} src={row.avatarUrl} />
+            {row.name}
+          </Box>
+        </TableCell>
+
+        <TableCell>{row.company}</TableCell>
+        <TableCell>{row.role}</TableCell>
+        <TableCell>{row.email}</TableCell>
+        <TableCell>{row.mobile}</TableCell>
+        {/* <TableCell>{row.billingAddress}</TableCell> */}
+
+        <TableCell align="center">
+          {row.isVerified ? (
+            <Iconify width={22} icon="solar:check-circle-bold" sx={{ color: 'success.main' }} />
+          ) : (
+            '-'
+          )}
+        </TableCell>
+
+        <TableCell>
+          <Label color={(row.status === 'locked' && 'error') || 'info'}>
+            {row.status}
+          </Label>
+        </TableCell>
+
+        <TableCell align="right">
+          <IconButton onClick={handleOpenPopover} value={row.id}>
+            <Iconify icon="eva:more-vertical-fill" />
+          </IconButton>
+        </TableCell>
+      </TableRow>
+
+      {/** Delete dialog */}
+      <Popover
+        open={!!openPopover}
+        anchorEl={openPopover}
+        onClose={() => setOpenPopover(null)}
+        anchorOrigin={{ vertical: 'top', horizontal: 'left' }}
+        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      >
+        <MenuList
+          disablePadding
+          sx={{
+            p: 0.5,
+            gap: 0.5,
+            width: 140,
+            display: 'flex',
+            flexDirection: 'column',
+            [`& .${menuItemClasses.root}`]: {
+              px: 1,
+              gap: 2,
+              borderRadius: 0.75,
+              [`&.${menuItemClasses.selected}`]: { bgcolor: 'action.selected' },
+            },
+          }}
+        >
+          <MenuItem onClick={handleEditing}>
+            <Iconify icon="solar:pen-bold" />
+          </MenuItem>
+
+          <MenuItem onClick={handleDelete} sx={{ color: 'error.main' }}>
+            <Iconify icon="solar:trash-bin-trash-bold" />
+          </MenuItem>
+        </MenuList>
+      </Popover>
+    </>
+  );
+}
+
+export default AgentTableRow;
---
+++ src/components/agent/AgentTableToolbar.tsx
@@ -0,0 +1,75 @@
+import IconButton from '@mui/material/IconButton';
+import InputAdornment from '@mui/material/InputAdornment';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import Toolbar from '@mui/material/Toolbar';
+import Tooltip from '@mui/material/Tooltip';
+import Typography from '@mui/material/Typography';
+import React from 'react';
+
+import { Iconify } from '../iconify';
+
+// ----------------------------------------------------------------------
+
+type AgentTableToolbarProps = {
+  numSelected: number;
+  filterName: string;
+  onFilterName: (evt: React.ChangeEvent<HTMLInputElement>) => void;
+  onMultipleDelete: (evt: React.MouseEvent<HTMLInputElement>) => void;
+};
+
+function AgentTableToolbar({
+  numSelected,
+  filterName,
+  onFilterName,
+  onMultipleDelete,
+}: AgentTableToolbarProps): React.ReactElement {
+  return (
+    <Toolbar
+      sx={{
+        height: 96,
+        display: 'flex',
+        justifyContent: 'space-between',
+        p: (theme) => theme.spacing(0, 1, 0, 3),
+        ...(numSelected > 0 && {
+          color: 'primary.main',
+          bgcolor: 'primary.lighter',
+        }),
+      }}
+    >
+      {numSelected > 0 ? (
+        <Typography component="div" variant="subtitle1">
+          {numSelected} selected
+        </Typography>
+      ) : (
+        <OutlinedInput
+          fullWidth
+          value={filterName}
+          onChange={onFilterName}
+          placeholder="Search user..."
+          startAdornment={
+            <InputAdornment position="start">
+              <Iconify width={20} icon="eva:search-fill" sx={{ color: 'text.disabled' }} />
+            </InputAdornment>
+          }
+          sx={{ maxWidth: 320 }}
+        />
+      )}
+
+      {numSelected > 0 ? (
+        <Tooltip title="Delete" onClick={onMultipleDelete}>
+          <IconButton>
+            <Iconify icon="solar:trash-bin-trash-bold" />
+          </IconButton>
+        </Tooltip>
+      ) : (
+        <Tooltip title="Filter list">
+          <IconButton>
+            <Iconify icon="ic:round-filter-list" />
+          </IconButton>
+        </Tooltip>
+      )}
+    </Toolbar>
+  );
+}
+
+export default AgentTableToolbar;
