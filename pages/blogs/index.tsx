326977c28d86f7ab590867a146a2f1be693a979f
Anastasiia <120492521+matoka02@users.noreply.github.com>
2025-04-10 10:58:42 +0300

    fix conflict packages
    update mui6.4.8->7.0.2, downgrate @toolpad/core0.13.0->0.10.0

--- pages/blogs/index.tsx
+++ pages/blogs/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable camelcase */
-import Grid from '@mui/material//Grid2';
+import Grid from '@mui/material//Grid';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Pagination from '@mui/material/Pagination';
