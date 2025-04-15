71d956eb0707b6605ff32f69159a668b84ffec9b
Anastasiia <120492521+matoka02@users.noreply.github.com>
2025-04-15 11:38:59 +0300

    update NotFoundView

--- pages/404/index.tsx
+++ pages/404/index.tsx
@@ -18,8 +18,8 @@ function NotFoundView() {
         </Typography>
 
         <Typography sx={{ color: 'text.secondary' }}>
-          Sorry, we couldn't find the page you're looking for. Perhaps you've mistyped the URL? Be
-          sure to check your spelling.
+          Sorry, we couldn&apos;t find the page you&apos;re looking for. Perhaps you&apos;ve
+          mistyped the URL? Be sure to check your spelling.
         </Typography>
 
         <Box
