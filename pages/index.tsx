b8bcf1a0024d4f281c596f78f40f4f1aad8155c5
Anastasiia <120492521+matoka02@users.noreply.github.com>
2025-04-16 19:08:26 +0300

    render dashboard view directly at root path

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,37 @@
+/* eslint-disable @typescript-eslint/no-unused-vars  */
 import Head from 'next/head';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 
+import { useAppRouter } from '@/routes/hooks';
+
+import OverviewAnalyticsView from './dashboard';
+
 export default function Home() {
-  const { t } = useTranslation();
+  // const { t } = useTranslation();
 
-  const onClick = () => {
-    setTimeout(() => {
-      // eslint-disable-next-line no-console
-      console.log('clicked');
-    }, 2000);
-  };
+  // const onClick = () => {
+  //   setTimeout(() => {
+  //     // eslint-disable-next-line no-console
+  //     console.log('clicked');
+  //   }, 2000);
+  // };
 
-  return (
-    <div>
-      <Head>
-        <title>Frontend SSR template</title>
-        <meta
-          name="description"
-          content="Frontend SSR template is used for bootstrapping a project."
-        />
-      </Head>
-      <button type="button" onClick={onClick}>
-        {t('click')}
-      </button>
-      <h1>Frontend SSR template</h1>
-    </div>
-  );
+  // return (
+  //   <div>
+  //     <Head>
+  //       <title>Frontend SSR template</title>
+  //       <meta
+  //         name="description"
+  //         content="Frontend SSR template is used for bootstrapping a project."
+  //       />
+  //     </Head>
+  //     <button type="button" onClick={onClick}>
+  //       {t('click')}
+  //     </button>
+  //     <h1>Frontend SSR template</h1>
+  //   </div>
+  // );
+
+  return <OverviewAnalyticsView />;
 }
